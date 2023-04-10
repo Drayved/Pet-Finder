a107096dab08bde5494d8f6fad9e499faae8c242
David Riley <Dril112191@gmail.com>
2023-04-10 16:47:57 -0400

    .

--- src/components/PictureCarousel.jsx
+++ src/components/PictureCarousel.jsx
@@ -1,64 +1,82 @@
 import React, { useState } from "react";
 
-export default function PictureCarousel ({ photos }) {
-    const [index, setIndex] = useState(0);
+export default function PictureCarousel({ photos }) {
+  const [index, setIndex] = useState(0);
 
   const prevIndex = index === 0 ? photos.length - 1 : index - 1;
   const nextIndex = index === photos.length - 1 ? 0 : index + 1;
 
+  const showArrows = photos.length > 1;
+
   return (
-    <div className="relative flex justify-center">
-      {photos.map((photo, i) => (
+    <div className="relative flex justify-center bg-[#050307] w-screen overflow-hidden">
+
+      {showArrows && (
         <img
-          key={i}
-          src={photo.medium}
-          alt={`Animal ${i + 1}`}
-          style={{ display: i === index ? "block" : "none" }}
-          className="h-96 w-72"
+          src={photos[prevIndex].medium}
+          alt={`Animal ${prevIndex + 1}`}
+          className="opacity-30 h-96 w-88 prev-image"
         />
-      ))}
+      )}
 
-      <img
-        src={photos[prevIndex].medium}
-        alt={`Animal ${prevIndex + 1}`}
-        className="absolute top-1/2 left-2 transform -translate-y-1/2 opacity-30 h-96 w-72"
-      />
+      <div className="relative">
+        {photos.map((photo, i) => (
+          <img
+            key={i}
+            src={photo.medium}
+            alt={`Animal ${i + 1}`}
+            style={{ display: i === index ? "inline-block" : "none", margin: 0 }}
+            className="h-96 w-88"
+          />
+        ))}
 
-      <img
-        src={photos[nextIndex].medium}
-        alt={`Animal ${nextIndex + 1}`}
-        className="absolute top-1/2 right-2 transform -translate-y-1/2 opacity-30 h-96 w-72"
-      />
+        <div className="flex justify-center absolute bottom-2 right-0 left-0 ">
+          {photos.map((_, i) => (
+            <div
+              key={i}
+              className={`h-2 w-2 rounded-full mx-1 ${i === index ? "bg-white" : "bg-gray-400"}`}
+            />
+          ))}
+        </div>
+      </div>
 
-      <button
-        onClick={() => setIndex(prevIndex)}
-        className="absolute top-1/2 left-2 transform -translate-y-1/2"
-      >
-        <svg
-          className="h-10 w-10 fill-current text-gray-500"
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 20 20"
+      {showArrows && (
+        <img
+          src={photos[nextIndex].medium}
+          alt={`Animal ${nextIndex + 1}`}
+          className="opacity-30 h-96 w-88 next-image"
+        />
+      )}
+
+      {showArrows && (
+        <button
+          onClick={() => setIndex(prevIndex)}
+          className="absolute top-1/2 left-2 transform -translate-y-1/2"
         >
-          <path d="M10 0L0 10h3v10h14V10h3L10 0z" />
-        </svg>
-      </button>
+          <svg
+            className="h-10 w-10 fill-current text-gray-500 -rotate-90 "
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 20 20"
+          >
+            <path d="M10 0L0 10h3v10h14V10h3L10 0z" />
+          </svg>
+        </button>
+      )}
 
-      <button
-        onClick={() => setIndex(nextIndex)}
-        className="absolute top-1/2 right-2 transform -translate-y-1/2"
-      >
-        <svg
-          className="h-10 w-10 fill-current text-gray-500"
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 20 20"
+      {showArrows && (
+        <button
+          onClick={() => setIndex(nextIndex)}
+          className="absolute top-1/2 right-2 transform -translate-y-1/2"
         >
-          <path d="M10 0L0 10h3v10h14V10h3L10 0z" />
-        </svg>
-      </button>
+          <svg
+            className="h-10 w-10 fill-current text-gray-500 rotate-90"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 20 20"
+          >
+            <path d="M10 0L0 10h3v10h14V10h3L10 0z" />
+          </svg>
+        </button>
+      )}
     </div>
   );
-  };
-  
-
-
-
+}
--- src/pages/Animal.jsx
+++ src/pages/Animal.jsx
@@ -1,42 +1,53 @@
 import React, { useContext, useState } from "react";
 import { AuthContext } from "../App";
 import { useParams } from "react-router-dom";
-import PictureCarousel from "../components/PictureCarousel"
-
-
-
-
-
+import PictureCarousel from "../components/PictureCarousel";
 
 export default function Animal() {
   const { results, filteredResults } = useContext(AuthContext);
   const { id } = useParams();
 
   const pets = filteredResults.length > 0 ? filteredResults : results;
-  console.log(pets)
-  // Find the animal object with the ID that matches the one in the URL
   const animal = pets.find((pet) => pet.id.toString() === id.toString());
 
   if (!animal) {
-    console.log("id:", id);
-    console.log("pets:", pets);
     return <div>Animal not found</div>;
   }
-
 
   return (
     <div>
       {animal && animal.photos && (
-  <>
-    <h2>{animal.name}</h2>
-    <PictureCarousel photos={animal.photos}/>
-    <p>Age: {animal.age}</p>
-    <p>Breed: {animal.breeds.primary}</p>
-    <p>Location: {animal.contact.address.city + ", " + animal.contact.address.state}</p>
-    <p>Description: {animal.description}</p>
-    {/* Display any other information that you want to show */}
-  </>
-)}
+        <div>
+          <PictureCarousel photos={animal.photos} />
+          <div className="border rounded-md mt-10 w-[600px] pl-5 ml-6 py-5">
+            <h2 className="text-3xl font-bold mb-4 ">{animal.name}</h2>
+            <div className="flex flex-col mt-4 ">
+              <p className="text-med my-5 font-semibold">
+                {animal.breeds.primary + " • " + animal.contact.address.city + ", " + animal.contact.address.state}
+              </p>
+              <p className="text-med border-y mr-5 py-3 ">
+                {animal.age + " • " + animal.gender + " • " + animal.size }
+              </p>
+              <h2 className="text-2xl my-5 font-bold">About</h2>
+              <p className="text-lg align-top mb-3">
+                {animal.attributes.spayed_neutered ? 'Spayed/Neutered' : 'Not spayed/neutered'}, 
+                {animal.attributes.house_trained ? ' House trained' : ' Not house trained'}
+                {animal.attributes.declawed && ', Declawed'}
+             </p>
+            </div>
+            {animal.description && (
+              <>
+                <h2 className="text-2xl border-t mr-5 pt-2 font-bold">Meet {animal.name}</h2>
+                <p className="text-sm mt-4 w-[550px]">
+                  {animal.description}
+               </p>
+              </>
+            )  
+          }
+            {/* Display any other information that you want to show */}
+          </div>
+        </div>
+      )}
     </div>
   );
 }
