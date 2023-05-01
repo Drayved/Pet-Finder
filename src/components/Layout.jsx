149620e4603785ce65f63481a01151fc8011e4f5
David Riley <Dril112191@gmail.com>
2023-05-01 12:18:51 -0400

    .

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -6,7 +6,7 @@ import { Outlet } from "react-router-dom"
 
 export default function Layout(){
     return(
-        <div>
+        <div className="relative min-h-[100vh] flex flex-col">
             <NavBar />
             <Header />
             <Outlet />
