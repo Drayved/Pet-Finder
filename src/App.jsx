02676c720a3d0c75b1ffa67ee6b444726968bb83
David Riley <Dril112191@gmail.com>
2023-07-20 13:11:34 -0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -24,7 +24,7 @@ function App() {
   useEffect(() => {
     const fetchToken = async () => {
       try {
-        const response = await fetch("https://attractive-mixed-apricot.glitch.me/");
+        const response = await fetch("https://davids-petfinder.netlify.app/.netlify/functions/server");
         const data = await response.json();
         setAccessToken(data.access_token);
         setIsLoading(false);
